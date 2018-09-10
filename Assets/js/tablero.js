b1a6bcae2f76ce3ddba640d0b7d96d92d56bc64d
Luc <noodle3030@gmail.com>
2018-09-10 18:16:28 -0500

    Update tablero.js

--- Assets/js/tablero.js
+++ Assets/js/tablero.js
@@ -5,20 +5,18 @@
 * Este metodo funciona sobre el tag <table id="tablero">
 */
 $(document).ready(function () {
-	for( var fila=0; fila<20;fila++)
-	{
+	for( var fila=0; fila<20;fila++){//abre for
 		var currentFila="F"+fila; //Almacena la fila actual
 		var crearFila = document.createElement("TR"); 
 	    //importante darle id a cada fila para que las columnas se impriman desde la fila 0 a la 20.
 	    crearFila.setAttribute("id", currentFila);
 	    document.getElementById("tablero").appendChild(crearFila); 
 
-		for(var columna=0;columna<20;columna++)
-		{
+		for(var columna=0;columna<20;columna++){//abre for
 		    var crearCuadro = document.createElement("TH");
 		    crearCuadro.classList.add("cuadro");
 		    //Dependiendo de la fila en la que se esta imprimir sus respectivas 20 columnas
 	   		document.getElementById(currentFila).appendChild(crearCuadro);
-		} 
-    }   
+		} //cierra for
+    }//cierra for   
 });
