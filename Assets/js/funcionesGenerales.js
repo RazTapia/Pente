6ddd26209a6138977d5317a650e4fb1f46114336
Tania T <tania.torresa@hotmail.com>
2018-09-20 20:09:57 -0500

    Se  cambia la variable de var a let

--- Assets/js/funcionesGenerales.js
+++ Assets/js/funcionesGenerales.js
@@ -54,9 +54,9 @@ function Ficha(x, y) {
 */
 
 function DibujarFichasTablero() {
-    for (i = 0; i <= 19; i++) {
-        for (j = 0; j <= 19; j++) {
-            Ficha(i,j);
+    for (let i = 0; i <= 19; i++) {
+        for (let j = 0; j <= 19; j++) {
+            Ficha(i, j);
         }
-    };
+    }
 }
