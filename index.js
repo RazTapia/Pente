b0815a9e065458fa85ab49faebdbccd2c5a6354c
Tania T <tania.torresa@hotmail.com>
2018-10-11 19:02:19 -0500

    Arquitectura cliente servidor y logica de usuarios

---
+++ index.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const socketIO= require('socket.io');
+var TOTAL_USERS;
+//Puerto
+app.set('port', process.env.PORT || 3000);
+
+//Routeo
+app.get('/', function(req, res) {
+
+  res.sendFile(path.join(__dirname,'public','login.html'));
+  });
+
+  app.get('/juego',function(req,res)
+  {  
+    if(TOTAL_USERS==2){ 
+      res.redirect('/error');
+   
+    }else{
+      console.log('juego '+ TOTAL_USERS);
+       res.sendFile(path.join(__dirname,'public','index.html'));
+    }
+  });
+  app.get('/error', function(req, res){
+    res.sendFile(path.join(__dirname,'public','error.html'));
+  });
+
+//Static
+app.use(express.static(path.join(__dirname,'public')));
+//test
+//Inicializa servidor
+
+const server = app.listen(app.get('port'),() =>{
+    console.log('server on port', app.get('port'));
+});
+
+//websockets
+const io=socketIO(server);
+
+io.on('connection',(socket) => {
+    console.log("Nueva conexion", socket.id);
+
+    TOTAL_USERS=io.engine.clientsCount;
+    //console.log(TOTAL_USERS);
+    if(io.engine.clientsCount>=3)
+    {
+     //console.log("ya son 2, no mas");   
+     socket.disconnect( true );
+    }
+    socket.on('disconnect', ()=>{
+      TOTAL_USERS=io.engine.clientsCount;
+      console.log('en dissconet'+TOTAL_USERS);	
+  })
+  
+  socket.on('pente:selecion',(data) => {
+    socket.broadcast.emit('pente:seleccion', data);
+  })
+});
+
--- public/assets/js/funcionesGenerales.js
+++ public/assets/js/funcionesGenerales.js
@@ -6,6 +6,8 @@
 * Este metodo funciona sobre el tag <table id="tablero">
 */
 
+const socket = io()
+
 function Tablero() {
 	for( var fila=0; fila<20; fila++) {
 		var filaActual="F"+fila; //Almacena la fila actual
@@ -46,10 +48,11 @@ function Ficha(x, y) {
 	var colorUsuario = "black"; //variable que contendrá el color de la ficha según el usuario,  Autor: LucNieto
 	let estadoFoo =0; //variable que contendrá el estado actual del hueco; 0 representa vacío y 1 representa ocupado, Autor: LucNieto
 	var crearFicha = document.createElement("DIV");
+	let idPadre=0;
     document.getElementById("F"+x+"C"+y).appendChild(crearFicha);
 	crearFicha.classList.add("ficha");
 	crearFicha.setAttribute("id",estadoFoo); //se le asigna un id al hueco para llevar control del estado de la ficha, Autor: LucNieto
-
+	crearFicha.setAttribute("draggable",false);
 /*
 * Autor: LucNieto
 * se obtiene el id del hueco para cambiar el color e indicar que se está seleccionando
@@ -59,10 +62,19 @@ function Ficha(x, y) {
 		(crearFicha.id == 0) ? (crearFicha.style.backgroundColor = "grey") : null ; }); // Autor: Lucio Nieto Bautista
 
 	crearFicha.addEventListener('mouseout', () => { (crearFicha.id == 0) ? crearFicha.style.backgroundColor = "lightgrey" : null});// Autor: Lucio Nieto Bautista
+		
+	crearFicha.addEventListener('mouseup', () => {
+		(crearFicha.id == 0) ? ( crearFicha.style.backgroundColor = colorUsuario, crearFicha.id = 1,
+			socket.emit('pente:selecion',{ id:crearFicha.parentNode.id}) )//Autor: Tania Torres Alvarado
+		: null;});// Fin  del bloque,Autor: Lucio Nieto Bautista
+	
+	}
 
-	crearFicha.addEventListener('click', () => {
-		(crearFicha.id == 0) ? ( crearFicha.style.backgroundColor = colorUsuario, crearFicha.id = 1) : null; });// Fin  del bloque,Autor: Lucio Nieto Bautista
-}
+socket.on('pente:seleccion',function(data){
+	var childNode =  document.getElementById(data.id).childNodes;
+	childNode[0].setAttribute('style', 'background-color: red;');
+	childNode[0].setAttribute('id', '1');
+});
 /*
 * Autor: Tania Torres Alvarado y Roberto Sagaón H.luz
 * Se integra el método que dibuja todas las fichas-hueco en el tablero que se
