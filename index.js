a2a1080dafbd59e45e05c3ff3d8448975518000c
Rztp <raziiiel95@hotmail.com>
2018-11-18 20:54:32 -0600

    Correcciones parciales Standard js

--- index.js
+++ index.js
@@ -1,76 +1,72 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const socketIO= require('socket.io');
-var TOTAL_USERS;
-var flag;
-//Puerto
-app.set('port', process.env.PORT || 3000);
+const path = require('path')
+const express = require('express')
+const app = express()
+const socketIO = require('socket.io')
+var TOTAL_USERS
+var flag
+// Puerto
+app.set('port', process.env.PORT || 3000)
 
-//Routeo
-app.get('/', function(req, res) {
+// Routeo
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'))
+})
 
-  res.sendFile(path.join(__dirname,'public','login.html'));
-  });
-
-  app.get('/juego',function(req,res)
-  {
-    if(TOTAL_USERS==2){
-      res.redirect('/error');
-
-    }else{
-      console.log('juego '+ TOTAL_USERS);
-       res.sendFile(path.join(__dirname,'public','index.html'));
-    }
-  });
-  app.get('/error', function(req, res){
-    res.sendFile(path.join(__dirname,'public','error.html'));
-  });
-
-//Static
-app.use(express.static(path.join(__dirname,'public')));
-//test
-//Inicializa servidor
-
-const server = app.listen(app.get('port'),() =>{
-    console.log('server on port', app.get('port'));
-});
-
-//websockets
-const io=socketIO(server);
-
-io.on('connection',(socket) => {
-  console.log("Nueva conexion", socket.id);
-  TOTAL_USERS=io.engine.clientsCount;
-
-  if(TOTAL_USERS==1){
-    socket.emit('jugador1', TOTAL_USERS);
+app.get('/juego', function (req, res) {
+  if (TOTAL_USERS == 2) {
+    res.redirect('/error')
+  } else {
+    console.log('juego ' + TOTAL_USERS)
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
   }
-  console.log(TOTAL_USERS);
-  if(TOTAL_USERS==2){
-    socket.broadcast.emit('jugador2', TOTAL_USERS);
+})
+app.get('/error', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'error.html'))
+})
+
+// Static
+app.use(express.static(path.join(__dirname, 'public')))
+// test
+// Inicializa servidor
+
+const server = app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'))
+})
+
+// websockets
+const io = socketIO(server)
+
+io.on('connection', (socket) => {
+  console.log('Nueva conexion', socket.id)
+  TOTAL_USERS = io.engine.clientsCount
+
+  if (TOTAL_USERS == 1) {
+    socket.emit('jugador1', TOTAL_USERS)
+  }
+  console.log(TOTAL_USERS)
+  if (TOTAL_USERS == 2) {
+    socket.broadcast.emit('jugador2', TOTAL_USERS)
   }
 
-  socket.on('disconnect', ()=>{
-    TOTAL_USERS=io.engine.clientsCount;
-    socket.broadcast.emit('desconectado', TOTAL_USERS);
+  socket.on('disconnect', () => {
+    TOTAL_USERS = io.engine.clientsCount
+    socket.broadcast.emit('desconectado', TOTAL_USERS)
   })
 
-  socket.on('pente:seleccion',(data) => {
-    socket.broadcast.emit('pente:seleccion', data);
+  socket.on('pente:seleccion', (data) => {
+    socket.broadcast.emit('pente:seleccion', data)
   })
 
-  socket.on('pente:comer',(data) => {
-    socket.broadcast.emit('pente:comeer', data);
+  socket.on('pente:comer', (data) => {
+    socket.broadcast.emit('pente:comeer', data)
   })
 
-  socket.on('pasarTiro',() => {
-    console.log("Servidor Metodo Evaluar Lineas 4");
-    socket.broadcast.emit('recibirTiro');
+  socket.on('pasarTiro', () => {
+    console.log('Servidor Metodo Evaluar Lineas 4')
+    socket.broadcast.emit('recibirTiro')
   })
 
   socket.on('perdedor', function (data) {
-  socket.broadcast.emit('perdedor', data);
+    socket.broadcast.emit('perdedor', data)
   })
-
-});
+})
